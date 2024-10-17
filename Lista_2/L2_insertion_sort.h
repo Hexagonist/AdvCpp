02d318246aeafe85325a71990a651f0355e6d892
Hexagonist <januszkrem2@wp.pl>
2024-10-17 17:01:25 +0200

    L2_test_1

--- Lista_2/L2_insertion_sort.h
+++ Lista_2/L2_insertion_sort.h
@@ -29,14 +29,33 @@ void insertion_sort<std::string>(std::vector<std::string>& vec_str)
     {
         for(size_t j = 0; j < i; j++)
         {
-            if(vec_str[i] < vec_str[j])   
-            {
-                T temp = vec_str[i];
-                vec_str[i] = vec_str[j];
-                vec_str[j] = temp;
-            }
+            /*
+             porównanie dwoch stringow
+             1. wyrzucamy takie same chary w stringach
+             2. jesli sa liczbami to sprawdzmy czy sa samotne
+             SPRAWDZAMY ktorymi sie roznia i porownujemy je
+            */ 
+
+           // Choose shorter string length to compare strings
+           size_t len = 0;
+           if (vec_str[i].size() <= vec_str[j].size()) len = vec_str[i].size();
+           else len = vec_str[j].size();
+
+           for(size_t k = 0; k < len; k++)
+           {
+            
+
+           }
+
+            // if(vec_str[i] < vec_str[j])   
+            // {
+            //     std::string temp = vec_str[i];
+            //     vec_str[i] = vec_str[j];
+            //     vec_str[j] = temp;
+            // }
         }
     }
+    // std::cout<<"dziala specyfikacja\n";
 }
 
 
--- Lista_2/Zad1.cpp
+++ Lista_2/Zad1.cpp
@@ -3,9 +3,15 @@
 
 int main()
 {
-    std::vector<std::string> vec_str({"ada", "baba", "celina"});
+    std::vector<std::string> vec_str({"ada", "basia", "celina", "2", "11", "1"});
+    std::vector<std::string> vec_str_2({"2", "11", "1"});
 
-    print_vector(vec_str);
+    print_vector(vec_str_2);
+    insertion_sort(vec_str_2);
+    print_vector(vec_str_2);
+
+    std::string str1 = "1abc";
+    std::cout<<str1[0];
 
     return 0;
 }
