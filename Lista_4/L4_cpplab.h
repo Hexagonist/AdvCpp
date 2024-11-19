c8de698f668fa3eb9fe6f10ea4b54633d5ce569b
Hexagonist <januszkrem2@wp.pl>
2024-11-20 00:02:39 +0100

    comments deleted

--- Lista_4/L4_cpplab.h
+++ Lista_4/L4_cpplab.h
@@ -12,16 +12,7 @@ namespace cpplab{
     {
         public:
             // Default constructor without memory allocation
-            vector() : maxSize(0), _size(0), begin(nullptr)
-            {
-                // std::cout<<"cpplab::vector created!"<<std::endl;
-            }
-
-            vector() : maxSize(0), _size(0), begin(nullptr)
-            {
-                // std::cout<<"cpplab::vector created!"<<std::endl;
-            }
-
+            vector() : maxSize(0), _size(0), begin(nullptr) {}
 
             // [] Operator overloading to acquire element of vector
             T& operator[](int a) const
@@ -38,22 +29,18 @@ namespace cpplab{
                     resize(1);
                     begin[0] = val;
                     _size++;
-                    // std::cout<<"begin==nullptr\n";
                 }
 
                 else if(_size < maxSize)
                 {
-                    // std::cout<<"Pushing back: "<<val<<"\n";
                     begin[_size] = val;
                     _size++;
-                    // std::cout<<"_size<\n";
                 }
                 else if (_size == maxSize)
                 {
                     resize(maxSize + 1);
                     begin[_size] = val;
                     _size++;
-                    // std::cout<<"_size==\n";
                 }
                 else 
                 {
@@ -66,10 +53,8 @@ namespace cpplab{
             {
                 if(_size > 0)
                 {
-                    // std::cout<<"Poping back"<<"\n";
                     _size--;
                     begin[_size] = 0;
-                    // std::cout<<"Popped val =  "<<begin[_size]<<"\n";
                 }
                 else
                 {
@@ -114,48 +99,49 @@ namespace cpplab{
             // Destructor of the vector 
             ~vector()
             {
-                // delete[] begin;
                 // std::cout<<"cpplab::vector destroyed!\n";
+                delete[] begin;
             }
 
             
+            using value_type = T; // Type of vector elements
         private:
             size_t maxSize; // Max size of vector
             size_t _size; // Current size of vector
             T* begin; // Pointer to first element of the vector
-            using value_type = T; // Type of vector elements
     };
 
-    template<typename T>
-    concept multiplicable = requires (T t){
-        // Check if type has size parameter
-        {t.size()} -> std::same_as<size_t>;
-        // Check if type is iterable
-        {t[0]};
-        // Check if type's values are numbers
-        requires std::is_arithmetic_v<std::remove_reference_t<decltype(t[0])>>;
-    };
-    
-    auto operator*(multiplicable auto L_type, multiplicable auto R_type)
-    {
-        if(L_type.size() == R_type.size())
-        {
-            auto product = 0;
-            size_t size = 0;
-
-            size = R_type.size();
-            for(size_t i = 0; i < size; i++)
-            {
-                product += L_type[i] * R_type[i];
-            }
-            return product;
-        }
-        else
-        {
-            throw std::runtime_error("Vectors must be the same size!");
-        }
-    }
 };
+
+template<typename T>
+concept multiplicable = requires (T t){
+    // Check if type has size parameter
+    {t.size()} -> std::same_as<size_t>;
+    // Check if type is iterable
+    {t[0]};
+    // Check if type's values are numbers
+    requires std::is_arithmetic_v<std::remove_reference_t<decltype(t[0])>>;
+};
+
+auto operator*(const multiplicable auto &L_type, const multiplicable auto &R_type)
+{
+    if(L_type.size() == R_type.size())
+    {
+        auto product = 0;
+        size_t size = 0;
+
+        size = R_type.size();
+        for(size_t i = 0; i < size; i++)
+        {
+            product += L_type[i] * R_type[i];
+        }
+        return product;
+    }
+    else
+    {
+        throw std::runtime_error("Vectors must be the same size!");
+    }
+}
 
 // Only to print cpplab::vector in comfortable way
 template<typename T>
@@ -178,3 +164,4 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
     }
     return os;
 }
+
