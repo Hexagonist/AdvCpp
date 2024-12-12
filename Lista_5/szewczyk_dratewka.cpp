08ef7c58d30de3388693caba72c6c2c3e6b0e3eb
Hexagonist <januszkrem2@wp.pl>
2024-12-12 14:44:49 +0100

    Szewczyk basics

--- Lista_5/szewczyk_dratewka.cpp
+++ Lista_5/szewczyk_dratewka.cpp
@@ -4,15 +4,41 @@
 #include <vector>
 
 
+std::vector<std::pair<unsigned int, unsigned int>> choose_best_treasures(
+    const unsigned int &treasures_amount,
+    const unsigned int &max_weight,
+    const std::vector<std::pair<unsigned int, unsigned int>> &treasures)
+{
+    std::vector<std::pair<unsigned int, unsigned int>> result;
+
+}
+
 
 int main() 
 {
-    std::cout<<"\nZadanie 2:\n";
-    std::string str = "I am a string";
-    // std::unique_ptr<std::string> standard_ptr = std::make_unique(str);
-    std::string  *standard_ptr = &str;
+    std::cout<<"\nZadanie 5 (Szewczyk Dratewka):\n";
 
-    std::cout<<standard_ptr->size()<<"\n";
+    const unsigned int N = 5, W = 30;
+
+    // treasure(weight, price)
+    const std::vector<std::pair<unsigned int, unsigned int>> treasures 
+    ({
+    std::pair(5, 100), 
+    std::pair(20, 300),
+    std::pair(2, 150), 
+    std::pair(5, 100), 
+    std::pair(5, 100)
+    });
+
+
+
+
+    
+    for(auto x : treasures)
+    {
+
+    }
+
 
 
 
