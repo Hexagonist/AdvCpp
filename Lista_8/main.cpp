b40eed4c11ebc273c58c580833b43b00f40dfd18
Hexagonist <januszkrem2@wp.pl>
2025-02-02 23:55:27 +0100

    Zad1 works

--- Lista_8/main.cpp
+++ Lista_8/main.cpp
@@ -1,11 +1,11 @@
 #include "singleton.h"
-// #include "calls1.h"
+#include "calls1.h"
 
 int main() {
     
     Singleton& singleton = Singleton::instance();
     singleton.calls();
-    // calls1();
+    calls1();
     
     return 0;
 }
